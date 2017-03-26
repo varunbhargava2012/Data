9ac852cda608d9ed973abab32f72c3953bde5c28
varunbhargava2012 <vsankuri@asu.edu>
2017-03-26 09:55:33 -0700

    Update HackDecision.py

--- HackDecision.py
+++ HackDecision.py
@@ -12,11 +12,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report,confusion_matrix
 from sklearn.model_selection import GridSearchCV
-
+#Decision tree
 df=pd.read_csv('Rating  (2).csv')
 X=df.drop(['Recommendation','Recommend Rating','State','City','Hospital Name','Provider ID'],axis=1)
 y=df['Recommendation']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
 dt=DecisionTreeClassifier(criterion='entropy')
 dt.fit(X_train,y_train)
 pred=dt.predict(X_test)
@@ -25,7 +26,7 @@ print('#Decision Tree results')
 print(classification_report(y_test,pred))
 
 
-
+#Random Forest classifier
 rfc=RandomForestClassifier(n_estimators=500)
 rfc.fit(X_train,y_train)
 pred=rfc.predict(X_test)
@@ -33,7 +34,7 @@ print(confusion_matrix(y_test,pred))
 print('#Random Forest Results')
 print(classification_report(y_test,pred))
 
-
+#Support vector machines
 svc=SVC()
 svc.fit(X_train,y_train)
 pred=svc.predict(X_test)
@@ -51,7 +52,36 @@ print(grid.best_params_)
 print(grid.best_estimator_)
 
 
+#state wise Geographcal mapping
+import plotly.plotly as py
+import plotly.graph_objs as go 
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
+init_notebook_mode(connected=True)
+
+
+df = pd.read_csv('Rating1.csv')
+
+data = dict(type='choropleth',
+            colorscale = 'Viridis',
+            locations = df['State'],
+            z = df['Recommend Rating'],
+            locationmode = 'USA-states',
+            #text = df['text'],
+            marker = dict(line = dict(color = 'rgb(255,255,255)',width = 2)),
+            colorbar = {'title':"Rating"}
+            )
+
+
+layout = dict(title = 'Hospital Rating',
+              geo = dict(scope='usa',
+                         showlakes = False,
+                         lakecolor = 'rgb(85,173,240)')
+             )
+
+choromap = go.Figure(data = [data],layout = layout)
+
+iplot(choromap)
 
 
 
